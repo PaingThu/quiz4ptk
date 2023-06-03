160296e3fed34dd647a5361b32aad421101c1ca5
Maw Paing Thu <paingmawthu@gmail.com>
2023-06-03 22:21:39 +0900

    Change Speech Synthesis to ResponsiveVoice

--- src/functions.js
+++ src/functions.js
@@ -1,6 +1,7 @@
 const ss = window.speechSynthesis
 export const textToSpeech = (text) => {
-    let speech = new SpeechSynthesisUtterance(text);
-    speech.voice = ss.getVoices()[153]
-    ss.speak(speech)
+    // let speech = new SpeechSynthesisUtterance(text);
+    // speech.voice = ss.getVoices()[153]
+    // ss.speak(speech)
+    responsiveVoice.speak(text, "Japanese Male",{pitch: 1, rate: 1});
 }
